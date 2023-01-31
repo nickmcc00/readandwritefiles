21c7922f15a257480efc2f21fda792392470e695
nickmcc00 <nicholas_mccullough1@baylor.edu>
2023-01-30 20:50:56 -0600

    finished

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -10,6 +10,10 @@ next(csvfile)
 
 outfile.write("Customer | Total\n")
 
+list_1 = []
+calc_total = 0
+cust_ID_1 = 0
+
 
 for sales in csvfile:
     customer_ID = int(sales[0])
@@ -17,12 +21,18 @@ for sales in csvfile:
     tax = float(sales[4])
     freight = float(sales[5])
 
-    calc_total = subtotal + tax + freight
+    if customer_ID != cust_ID_1:
+        if cust_ID_1 != 0:
+            list_1.append([cust_ID_1, calc_total])
 
-    outfile.write(
-        "\t" + str(customer_ID) + "\t" + str(format(calc_total, ">9.2f")) + "\n"
-    )
-    # if dict['customer_ID'] exists
+        cust_ID_1 = customer_ID
+
+        calc_total = subtotal + tax + freight
+    else:
+        calc_total += subtotal + tax + freight
+
+for sales in list_1:
+    outfile.write("\t" + str(sales[0]) + "\t" + format(sales[1], ">11.2f") + "\n")
 
 
 outfile.close()
